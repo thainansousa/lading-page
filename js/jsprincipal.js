96cddd9f9864a566d604c990d0debd619c441521
thainansousa <63759369+thainansousa@users.noreply.github.com>
2020-05-03 10:27:56 -0300

    Add files via upload

---
+++ js/jsprincipal.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+	$('.md-50 .form').submit(function(){
+		alert('\u{274C} Olá, essa página ela é um exemplo, os botões eles são apenas ilustrações!')
+		return false;
+	})
+	$('.banner-principal .md-50 a').click(function(){
+		alert('\u{274C} Olá, essa página ela é um exemplo, os botões eles são apenas ilustrações!')
+		return false;
+	})
+	$('.newsletter-section .newsletter').submit(function(){
+		alert('\u{274C} Olá, essa página ela é um exemplo, os botões eles são apenas ilustrações!')
+		return false;
+	})
+
+
+});
